4a87170bcf21a91134c84f9ecf7d53a7949287a3
Rohit Jain <rohitjain3241@gmail.com>
2017-12-02 17:56:26 +0530

    Adding couple of solutions

---
+++ BFS/Easy/Minimum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+
+//trying a basic recursive approach
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if(root==NULL)
+            return 0;
+        else if(root->left==NULL)
+            return 1 + minDepth(root->right);
+        else if(root->right==NULL)
+            return 1 + minDepth(root->left);
+        else 
+            return 1 + min(minDepth(root->left),minDepth(root->right));
+    }
+};
